1444c0b5447e6908cb626bfbf91e3dee8a7d5110
GehaziJenda <gehazijenda@gmail.com>
2023-02-16 13:38:07 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'printing'
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
