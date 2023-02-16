5d0e4a21a20ca69f66c9e3b87fc201f0d2d7ef2c
GehaziJenda <gehazijenda@gmail.com>
2023-02-16 14:04:05 -0800

    Added number 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Showing sizes of objects'
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int integer;
+	char character;
+	long big_integer;
+	float floating_integer;
+	int long_integer;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(character));
+	printf("Size of an int: %lu byte(s)\n", sizeof(integer));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long_integer));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(big_integer));
+	printf("Size of a float: %lu byte(s)\n", sizeof(floating_integer));
+	return (0);
+}
