ba69ccd2eb44d0bb47aa2f314bc2fea2574e26fc
GehaziJenda <gehazijenda@gmail.com>
2023-02-17 14:00:16 -0800

    Alphabet missing letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Prints the alphabet'
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int i;
+	char letters[24] = {
+		'a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'q', 'r', 's', 't',
+		 'u', 'v', 'w', 'x', 'y', 'z'
+	};
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(letters[i]);
+	}
+	putchar('\n');
+	return (0);
+}
