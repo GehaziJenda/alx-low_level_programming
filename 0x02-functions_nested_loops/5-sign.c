546cc29af2ee13e042a8ed0b89594bc059375083
GehaziJenda <gehazijenda@gmail.com>
2023-02-21 14:10:31 -0800

    Checking if positive

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - Checks
+ * Description: 'Checks if number is positive or negative'
+ * @n: character to evaluate
+ * Return: 1 if positive, 0 if zero and -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		i = 1;
+	}
+	else
+	{
+		if (n == 0)
+		{
+			i = 0;
+		}
+		else
+		{
+			i = -1;
+		}
+	}
+	return (i);
+}
