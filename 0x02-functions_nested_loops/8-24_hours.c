1b072444edf165d069686f99ceff42b23ff005ea
GehaziJenda <gehazijenda@gmail.com>
2023-02-21 15:25:54 -0800

    Another

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+void check_time(int c);
+
 /**
  * jack_bauer - check the code
  *
@@ -15,37 +17,28 @@ void jack_bauer(void)
 
 		for (minute = 0; minute < 60; minute++)
 		{
-			if (hour < 10)
-			{
-				_putchar('0');
-				_putchar(hour + '0');
-				_putchar(':');
-				if (minute < 10)
-				{
-					_putchar('0');
-					_putchar(minute + '0');
-				}
-				else
-				{
-					_putchar(minute + '0');
-				}
-			}
-			else
-			{
-				_putchar(hour + '0');
-				_putchar(':');
-				if (minute < 10)
-				{
-					_putchar('0');
-					_putchar(minute + '0');
-				}
-				else
-				{
-					_putchar(minute + '0');
-				}
-
-			}
+			check_time(hour);
+			_putchar(':');
+			check_time(minute);
 			_putchar('\n');
 		}
+	}
+}
+
+/**
+ * check_time - check the code
+ * @c: value to be evaluated
+ * Return: void.
+ */
+void check_time(int c)
+{
+	if (c < 10)
+	{
+		_putchar('0');
+		_putchar(c + '0');
+	}
+	else
+	{
+		_putchar(c + '0');
 	}
 }
