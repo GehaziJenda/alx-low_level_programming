2c2207016702e77fdfbe5716a0cb0994c6404e76
GehaziJenda <gehazijenda@gmail.com>
2023-02-21 15:03:22 -0800

    24 hour clock

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * jack_bauer - check the code
+ *
+ * Return: void.
+ */
+void jack_bauer(void)
+{
+	int hour;
+
+	for (hour = 0; hour < 24; i++)
+	{
+		int minute;
+
+		for (minute = 0; minute < 60; minute++)
+		{
+			if (hour < 10)
+			{
+				_putchar('0');
+				_putchar(hour + '0');
+				if (minute < 10)
+				{
+					_putchar('0');
+					_putchar(minute + '0');
+				}
+				else
+				{
+					_putchar(minute + '0');
+				}
+			}
+			else
+			{
+				_putchar(hour + '0');
+				if (minute < 10)
+				{
+					_putchar('0');
+					_putchar(minute + '0)
+				}
+				else
+				{
+					_putchar(minute + '0');
+				}
+
+			}
+			_putchar('\n');
+		}
+	}
+}
