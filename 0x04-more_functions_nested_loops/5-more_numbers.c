60f5ce67ee6197a3e3d1eb56f1763d896d466972
GehaziJenda <gehazijenda@gmail.com>
2023-02-23 12:08:17 -0800

    Repeating numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - Checks
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		int j;
+
+		for (j = 0; j <= 14; j++)
+		{
+			int first = j / 10;
+			int last = j % 10;
+
+			if (first > 0)
+			{
+				_putchar(first + '0');
+			}
+			_putchar(last + '0');
+		}
+		_putchar('\n');
+	}
+}
