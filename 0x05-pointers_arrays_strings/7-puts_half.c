75f5afce44e4a0c40b2e933528787f3319dee2e3
GehaziJenda <gehazijenda@gmail.com>
2023-02-28 10:00:08 -0800

    Printing half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * puts_half - Checks
+ * @str: character to evaluate
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	char s = ' ';
+	int i = 0;
+	int j = 0;
+
+	while (s != '\0')
+	{
+		s = str[i];
+		if (s != '\0')
+		{
+			i++;
+		}
+	}
+	
+	if (i % 2 == 0)
+	{
+		for (j = i / 2; j < i; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	else
+	{
+		for (j = (i - 1) / 2; j < i; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
+}
