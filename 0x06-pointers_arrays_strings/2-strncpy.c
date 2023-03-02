124a852e15a7b30e9ef74f7b53e65138c7959212
GehaziJenda <gehazijenda@gmail.com>
2023-03-02 10:58:49 -0800

    3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,41 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	char *result = dest;
 	int i = 1;
+	char srcLast = ' ';
+	int j = 1;
 
-	while (*src != '\0' && i <= n)
+	while (srcLast != '\0')
 	{
-		*dest = *src;
-		dest++;
-		src++;
-		i++;
+		srcLast = src[i];
+		if (srcLast != '\0')
+		{
+			i++;
+		}
+	}
+	if (i > n)
+	{
+		while (j <= n)
+		{
+			*dest = *src;
+			j++;
+			dest++;
+			src++;
+		}
+		while (*dest != '\0')
+		{
+			*dest = *dest;
+			dest++;
+		}
+	}
+	else
+	{
+		while (j <= n)
+		{
+			*dest = *src;
+			j++;
+			dest++;
+			src++;
+		}
 	}
 	*dest = '\0';
 	return (result);
