04a75e6a14709485e5a3cdc753f6fd0639997c53
GehaziJenda <gehazijenda@gmail.com>
2023-03-08 14:49:32 -0800

    Power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns value of x raised to y
+ * @x: first variable
+ * @y: second variable
+ * Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
